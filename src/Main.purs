d977424b751720912b5ac732135ffccab135e7a8
Matias <selinmatias5@gmail.com>
2023-12-22 15:13:51 +0200

    create gameinstance from json

--- src/Main.purs
+++ src/Main.purs
@@ -7,6 +7,7 @@ import Affjax.RequestHeader (RequestHeader(..))
 import Affjax.ResponseFormat as RF
 import Affjax.Web (defaultRequest)
 import Control.Monad.Except (runExcept)
+import Data.Argonaut (Json, JsonDecodeError, decodeJson, jsonParser)
 import Data.Either (Either(..))
 import Data.HTTP.Method as Method
 import Data.Maybe (Maybe(..))
@@ -23,7 +24,7 @@ import Web.Socket.Event.EventTypes (onMessage, onOpen)
 import Web.Socket.Event.MessageEvent (fromEvent, data_)
 import Web.Socket.WebSocket as WS
 
-data GameInstance = GameInstance {
+type GameInstance = {
   gameState :: String,
   owner :: String,
   status :: String,
@@ -31,9 +32,6 @@ data GameInstance = GameInstance {
   gameType :: String,
   entityId :: String
 }
-
-instance showGameInstance :: Show GameInstance where
-  show (GameInstance { gameState, owner, status, createdAt, gameType, entityId }) = "GameInstance { gameState: " <> gameState <> ", owner: " <> owner <> ", status: " <> status <> ", createdAt: " <> createdAt <> ", gameType: " <> gameType <> ", entityId: " <> entityId <> " }"
 
 main :: Effect Unit
 main = do
@@ -57,29 +55,36 @@ runStuff playerToken levelID = do
 
   runAff_ (\either -> case either of
     Left error -> log $ show error
-    Right game -> logShow game
+    Right game -> do
+      log $ "game created"
+      connection <- WS.create ("wss://goldrush.monad.fi/backend/" <> playerToken <>  "/") []
+      logShow =<< WS.readyState connection
+
+      let socket = WS.toEventTarget connection
+
+      openListener <- eventListener(\event -> do
+        log "Connection opened"
+        WS.sendString connection ("[\"sub-game\",{\"id\":" <> levelID <> "}]"))
+      messageListener <- eventListener(messageReceiver)
+
+      addEventListener onOpen openListener true socket
+      addEventListener onMessage messageListener true socket
   ) do
     createGame playerToken levelID
 
-  connection <- WS.create ("wss://goldrush.monad.fi/backend/" <> playerToken <>  "/") []
-  logShow =<< WS.readyState connection
+gameInstanceFromJson :: Json -> Either JsonDecodeError GameInstance
+gameInstanceFromJson = decodeJson
 
-  let socket = WS.toEventTarget connection
-
-  openListener <- eventListener(\event -> do
-    log "Connection opened"
-    WS.sendString connection ("[\"sub-game\",{\"id\":" <> levelID <> "}]"))
-  messageListener <- eventListener(messageReceiver)
-
-  addEventListener onOpen openListener true socket
-  addEventListener onMessage messageListener true socket
-
-createGame ∷ String → String → Aff String
+createGame :: String -> String -> Aff (Either String GameInstance)
 createGame playerToken levelID = do
   result <- AN.request (defaultRequest { url = ("https://goldrush.monad.fi/backend/api/levels/" <> levelID), method = Left Method.POST, headers = [(RequestHeader "Authorization" playerToken)], responseFormat = RF.string })
   case result of
-    Left err -> pure "GET /api response failed to decode"
-    Right response -> pure response.body
+    Left err -> pure (Left "GET /api response failed to decode")
+    Right response -> case jsonParser response.body of
+      Left err -> pure (Left "failed to parse json")
+      Right game -> case gameInstanceFromJson game of
+        Left e -> pure (Left "failed to decode json")
+        Right gameInstance -> pure (Right gameInstance)
 
 messageReceiver :: Event -> Effect Unit
 messageReceiver event = do
